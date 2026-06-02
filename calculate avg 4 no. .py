4a800eb8f7ac29b40ca5a26e0886c66960a20872
jagrutipanigrahy <jagrutis2007@gmail.com>
2026-06-02 19:26:10 +0530

    Add files via upload

---
+++ calculate avg 4 no. .py
@@ -0,0 +1,6 @@
+num1=float(input("enter first number:"))
+num2=float(input("enter second number:"))
+num3=float(input("enter third number:"))
+num4=float(input("enter fourth number"))
+average=num1+num2+num3+num4/4
+print("average=",average)
