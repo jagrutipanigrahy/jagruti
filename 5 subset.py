51b038978ad6fc95d8cedb803db8fabdae81c23e
jagrutipanigrahy <jagrutis2007@gmail.com>
2026-06-02 19:25:33 +0530

    Add files via upload

---
+++ 5 subset.py
@@ -0,0 +1,9 @@
+s1=float(input("enter the marks of studens 1:"))
+s2=float(input("enter marks of student 2:"))
+s3=float(input("enter marks of subject3:"))
+s4=float(input("enter marks of subject4:"))
+s5=float(input("enter marks of subject5:"))
+total=s1+s2+s3+s4+s5
+percentage=(total/500)*100
+print("total marks=",total)
+print("percentage=",percentage,"%")
